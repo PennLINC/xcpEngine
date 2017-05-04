2709f0ef7d929d59b5ba02973bf3700937821bef
Rastko Ciric <rciric@chead.uphs.upenn.edu>
2017-05-04 16:43:36 -0400

    20170504: readability and comments

--- core/checkRpackageDependencies.R
+++ core/checkRpackageDependencies.R
@@ -7,15 +7,34 @@
 ###################################################################
 # Check all R package dependencies
 ###################################################################
-
 failed=FALSE
-if(!is.element('ANTsR', installed.packages()[,1])) { cat(' :::Dependencies check failed: ANTsR\n :::Please add ANTsR to your R installation\n'); failed=TRUE }
-if(!is.element('optparse', installed.packages()[,1])) { cat(' :::Dependencies check failed: optparse\n :::Please add optparse to your R installation\n'); failed=TRUE }
-if(!is.element('pracma', installed.packages()[,1])) { cat(' :::Dependencies check failed: pracma\n :::Please add pracma to your R installation\n'); failed=TRUE }
-if(!is.element('signal', installed.packages()[,1])) { cat(' :::Dependencies check failed: signal\n :::Please add signal to your R installation\n'); failed=TRUE }
+if(!is.element('ANTsR', installed.packages()[,1])) {
+   cat(' :::Dependencies check failed: ANTsR\n :::Please add ANTsR to your R installation\n')
+   failed=TRUE
+}
+if(!is.element('optparse', installed.packages()[,1])) {
+   cat(' :::Dependencies check failed: optparse\n :::Please add optparse to your R installation\n')
+   failed=TRUE
+}
+if(!is.element('pracma', installed.packages()[,1])) {
+   cat(' :::Dependencies check failed: pracma\n :::Please add pracma to your R installation\n')
+   failed=TRUE
+}
+if(!is.element('signal', installed.packages()[,1])) {
+   cat(' :::Dependencies check failed: signal\n :::Please add signal to your R installation\n')
+   failed=TRUE
+}
 
-if (failed==TRUE) { quit() }
+###################################################################
+# Exit if any dependency is unsatisfied.
+###################################################################
+if (failed==TRUE) {
+   quit()
+}
 
+###################################################################
+# Obtain and print versions of all package dependencies.
+###################################################################
 ver_ANTSR <- packageVersion("ANTsR")
 ver_OPTPARSE <- packageVersion("optparse")
 ver_PRACMA <- packageVersion("pracma")
