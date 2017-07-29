2943c76401220f53b098e947f40c76538c852bb5
rciric <rc042010@mymail.pomona.edu>
2017-07-28 22:29:13 -0400

    20170728: resolves #39

--- core/functions/library.sh
+++ core/functions/library.sh
@@ -11,12 +11,15 @@ LIBRARY=${XCPEDIR}/core/functions
 
 source ${LIBRARY}/add_reference
 source ${LIBRARY}/arithmetic
+source ${LIBRARY}/atlas_add
+source ${LIBRARY}/atlas_config
 source ${LIBRARY}/atlas_parse
 source ${LIBRARY}/cleanup
 source ${LIBRARY}/configure
 source ${LIBRARY}/derivative
 source ${LIBRARY}/derivative_parse
 source ${LIBRARY}/derivative_typeof
+source ${LIBRARY}/echo_cmd
 source ${LIBRARY}/exec_afni
 source ${LIBRARY}/exec_ants
 source ${LIBRARY}/exec_c3d
@@ -31,6 +34,7 @@ source ${LIBRARY}/is_integer
 source ${LIBRARY}/is+integer
 source ${LIBRARY}/is_numeric
 source ${LIBRARY}/is+numeric
+source ${LIBRARY}/json_query
 source ${LIBRARY}/load_atlas
 source ${LIBRARY}/load_derivatives
 source ${LIBRARY}/load_networks
@@ -38,9 +42,6 @@ source ${LIBRARY}/load_transforms
 source ${LIBRARY}/nat2native
 source ${LIBRARY}/nat2standard
 source ${LIBRARY}/nat2structural
-source ${LIBRARY}/network
-source ${LIBRARY}/network_add
-source ${LIBRARY}/network_parse
 source ${LIBRARY}/output
 source ${LIBRARY}/process
 source ${LIBRARY}/processed
@@ -58,6 +59,7 @@ source ${LIBRARY}/str2standard
 source ${LIBRARY}/str2structural
 source ${LIBRARY}/subroutine
 source ${LIBRARY}/verbose
+source ${LIBRARY}/write_atlas
 source ${LIBRARY}/write_config_safe
 source ${LIBRARY}/write_config
 source ${LIBRARY}/write_derivative
