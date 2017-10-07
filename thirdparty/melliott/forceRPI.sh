c6b8fd72ab130a57f68b236410c9c19b64db1fd0
rciric <rc042010@mymail.pomona.edu>
2017-10-07 15:58:49 -0400

    20171007: script import -- force RPI orientation

--- thirdparty/melliott/forceRPI.sh
+++ thirdparty/melliott/forceRPI.sh
@@ -1,50 +1,85 @@
 #!/usr/bin/env bash
 
-
 ###################################################################
 # Adaptation of Dr. Mark Elliott's force_RPI.sh script
 ###################################################################
+
+###################################################################
+# Constants
+###################################################################
+source ${XCPEDIR}/core/constants
+source ${XCPEDIR}/core/functions/library.sh
+
+###################################################################
+# Usage function
+###################################################################
+Usage(){
+cat << endstream
+___________________________________________________________________
+
+
+Usage: forceRPI <input> <output>
+
+forceRPI converts the input image to RPI orientation:
+q/sform_xorient  Right-to-Left
+q/sform_yorient  Posterior-to-Anterior
+q/sform_zorient  Inferior-to-Superior
+
+forceRPI is an adaptation of Dr. Mark Elliott's force_RPI.sh script
+
+endstream
+}
+
 input=$1
 output=$2
 orient=$($AFNI_PATH/@GetAfniOrient ${input})
 
 case ${orient} in
     RAI)
-        echo "Converting RAI to RPI orientation."
-        fslreorient2std ${input} ${output}
-        fslswapdim ${output} -x y z ${output}   # This intermediate result has INCONSISTENT data/header!!
-        fslorient -swaporient ${output}              # now it is correct
+        subroutine         @u.1              Converting RAI to RPI orientation
+        exec_fsl           fslreorient2std   ${input} ${output}
+        exec_fsl           fslswapdim        ${output} \
+                           -x y z            ${output}
+                           # This intermediate result has INCONSISTENT data/header!!
+        exec_fsl           fslorient         -swaporient ${output}
+                           # now it is correct
         ;;
     RAS)
-        echo "Converting RAS to RPI orientation."
-        fslreorient2std ${input} ${output}
+        subroutine         @u.2              Converting RAS to RPI orientation
+        exec_fsl           fslreorient2std   ${input} ${output}
         ;;
     RSA)
-        echo "Converting RSA to RPI orientation."
-        fslreorient2std ${input} ${output}
-        fslswapdim ${output} -x y z ${output}   # This intermediate result has INCONSISTENT data/header!!
-        fslorient -swaporient ${output}              # now it is correct
+        subroutine         @u.3              Converting RSA to RPI orientation
+        exec_fsl           fslreorient2std   ${input} ${output}
+        exec_fsl           fslswapdim        ${output} \
+                           -x y z            ${output}
+                           # This intermediate result has INCONSISTENT data/header!!
+        exec_fsl           fslorient         -swaporient ${output}
+                           # now it is correct
        ;;
     RIA)
-        echo "Converting RIA to RPI orientation."
-        fslreorient2std ${input} ${output}
+        subroutine         @u.4              Converting RAI to RPI orientation
+        exec_fsl           fslreorient2std   ${input} ${output}
         ;;
     ASR)
-        echo "Converting ASR to RPI orientation."
-        fslreorient2std ${input} ${output}
+        subroutine         @u.5              Converting ASR to RPI orientation
+        exec_fsl           fslreorient2std   ${input} ${output}
         ;;
     AIL)
-        echo "Converting AIL to RPI orientation."
-        fslreorient2std ${input} ${output}
+        subroutine         @u.6              Converting AIL to RPI orientation
+        exec_fsl           fslreorient2std   ${input} ${output}
         ;;
     ASL)
-        echo "Converting ASL to RPI orientation."
-        fslreorient2std ${input} ${output}
-        fslswapdim ${output} -x y z ${output}   # This intermediate result has INCONSISTENT data/header!!
-        fslorient -swaporient ${output}              # now it is correct
+        subroutine         @u.7              Converting ASL to RPI orientation
+        exec_fsl           fslreorient2std   ${input} ${output}
+        exec_fsl           fslswapdim        ${output} \
+                           -x y z            ${output}
+                           # This intermediate result has INCONSISTENT data/header!!
+        exec_fsl           fslorient         -swaporient ${output}
+                           # now it is correct
         ;;
     LPI)
-        echo "Converting LPI to RPI orientation."
+        subroutine         @u.8              Converting LPI to RPI orientation
         fslswapdim ${input} -x y z ${output}   			# This intermediate result has INCONSISTENT data/header!!
         fslorient -swaporient ${output}              # now it is correct
         ;;
@@ -53,10 +88,10 @@ case ${orient} in
         echo "Applying 3dresample : unexpected results are possible."
         echo "Oblique data will not be handled appropriately."
         echo "Please verify that the output is correctly oriented."
-        $AFNI_PATH/3dresample \
-            -overwrite \
-            -orient RPI \
-            -inset ${input} \
-            -prefix ${output}
+        exec_afni    3dresample  \
+            -overwrite           \
+            -orient  RPI         \
+            -inset   ${input}    \
+            -prefix  ${output}
         ;;
 esac
