e2d110de0f6a01141b0e818ba9ac08cc177c550f
rciric <rc042010@mymail.pomona.edu>
2017-10-10 22:58:21 -0400

    20171010: option to enforce expected label set in time series production

--- utils/roi2ts.R
+++ utils/roi2ts.R
@@ -26,6 +26,9 @@ option_list = list(
    make_option(c("-i", "--img"), action="store", default=NA, type='character',
               help="Path to the 4d BOLD timeseries from which the timeseries
                   will be extracted."),
+   make_option(c("-l", "--labels"), action="store", default=NA, type='character',
+              help="A list of indices corresponding to the regions from
+                  which time series are to be extracted."),
    make_option(c("-r", "--roi"), action="store", default=NA, type='character',
               help="A 3D image specifying the nodes or regions of interest
                   from which timeseries are to be extracted.")
@@ -45,6 +48,9 @@ if (is.na(opt$roi)) {
 
 impath                  <- opt$img
 roipath                 <- opt$roi
+if (! is.na(opt$labels)) {
+   labs                 <- as.vector(unlist(read.table(opt$labels)))
+} else { labs           <- NA }
 
 ###################################################################
 # 1. Load in the image.
@@ -63,7 +69,10 @@ net                     <- readNifti(roipath)
 #
 # First, obtain all unique nonzero values in the mask.
 ###################################################################
-labs                    <- sort(unique(net[net > 0]))
+labels                  <- sort(unique(net[net > 0]))
+if (is.na(labs[1])) {
+   labs                 <- labels
+}
 ###################################################################
 # Create a logical over all voxels, indicating whether each
 # voxel has a nonzero mask value.
@@ -105,22 +114,25 @@ if (length(labs) == 1) {
    # RoI timeseries is computed and added to the model.
    ################################################################
    nodevec              <- net[logmask]
-   voxelwise            <- mat[, nodevec == labs[1]]
-   if (is.null(dim(voxelwise)) && !is.null(length(voxelwise))) {
-      warning("Warning: node 1 contains one voxel\n")
-      dim(voxelwise)    <-c(length(voxelwise),1)
+   if (labs[1] %in% labels) {
+      voxelwise         <- mat[, nodevec == labs[1]]
+      if (is.null(dim(voxelwise)) && !is.null(length(voxelwise))) {
+         warning("Warning: node 1 contains one voxel\n")
+         dim(voxelwise) <-c(length(voxelwise),1)
+      }
+      mmat[,1]          <- matrix(apply(voxelwise, FUN = mean, MARGIN = 1), ncol = 1)
    }
-   mmat[,1]       <- matrix(apply(voxelwise, FUN = mean, MARGIN = 1), ncol = 1)
    ################################################################
    # Repeat for all remaining RoIs.
    ################################################################
    for (i in 2:length(labs)) {
+      if (! labs[i] %in% labels) { next }
       voxelwise         <-mat[, nodevec == labs[i]]
       if (is.null(dim(voxelwise)) && !is.null(length(voxelwise))) {
          warning(paste("Warning: node ", labs[i], " contains one voxel\n"))
          dim(voxelwise) <-c(length(voxelwise),1)
       }
-      mmat[,i]    <- matrix(apply(voxelwise, FUN = mean, MARGIN = 1), ncol = 1)
+      mmat[,i]          <- matrix(apply(voxelwise, FUN = mean, MARGIN = 1), ncol = 1)
    }
    colnames(mmat)       <- paste("L", labs)
 }
