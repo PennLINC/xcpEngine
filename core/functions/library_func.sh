4b6479308a44092e28974fbb05f58f3fbca72fef
rciric <rc042010@mymail.pomona.edu>
2017-10-29 17:50:38 -0400

    20171029: prime generalised time series processing

--- core/functions/library_func.sh
+++ core/functions/library_func.sh
@@ -11,9 +11,9 @@ LIBRARY=${XCPEDIR}/core/functions
 
 source ${LIBRARY}/demean_detrend
 source ${LIBRARY}/filter_temporal
-source ${LIBRARY}/filter_temporal_prime
 source ${LIBRARY}/remove_outliers
 source ${LIBRARY}/smooth_spatial
 source ${LIBRARY}/smooth_spatial_prime
 source ${LIBRARY}/temporal_mask
 source ${LIBRARY}/temporal_mask_prime
+source ${LIBRARY}/ts_process_prime
