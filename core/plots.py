5853b76cb139f49e9b782202a1c52f23dc8619fc
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2019-11-14 18:23:56 -0500

    Update plots.py

--- core/plots.py
+++ core/plots.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-"""Plotting tools shared across MRIQC and FMRIPREP"""
+"""Plotting tools """
 
 import numpy as np
 import nibabel as nb
